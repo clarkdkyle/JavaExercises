16545effc90a12f47ba00675b15c21a31cff49c4
clarkdkyle <clarkdkyle@gmail.com>
2021-03-09 23:08:50 -0600

    completed collections and finished Java 1

--- src/java_1/collections/CollectionsChallenges.java
+++ src/java_1/collections/CollectionsChallenges.java
@@ -3,19 +3,33 @@ package java_1.collections;
 import java.util.*;
 
 public class CollectionsChallenges {
-	static int[] nums = new int[]{10,23,3,4,5,2,1};
+    static Integer[] numbArr = {3,7,37,73};
+    static List<String> stateList = Arrays.asList("Kansas", "Colorado", "Oklahoma", "Kansas");
+    static ArrayList<Integer> numList = new ArrayList<>(Arrays.asList(3,7,37,73));
+    static LinkedList<String> linkedList = new LinkedList<>(Arrays.asList("red","orange","yellow","green","blue","indigo","violet"));
+    static ArrayList<String> names = new ArrayList<>(Arrays.asList("Smurf", "Malkovich", "Hodor", "Smurf", "Malkovich", "Hodor", "Pikachu"));
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 //		Complete the code challenges for the following static methods
+        findVal(2, numList);
+        System.out.println(convertArrToList(numbArr));
+        System.out.println(listToSet(stateList));
+        System.out.println(addToList(307, numList));
+        System.out.println(removeFromList(3, numList));
+        System.out.println(insertIntoList(7, 0, numList));
+        System.out.println(reverseNums(numList));
+        System.out.println(reverseLinkedList(linkedList, true));
+	    System.out.println(removeDuplicates(names));
 	}
 
-	int findVal(int val, ArrayList<Integer> arrList) {
+	static int findVal(int val, ArrayList<Integer> arrList) {
 //		find a value in the input array list.  return it if found.  If not, return 0.
+        if(arrList.contains(val)) {
+            return val;
+        }
 
-
-		return arrList.get(val);
-	}
-
+        else return 0;
+    }
 	static List convertArrToList(Object[] array) {
 //		convert the array to a list and return it.
 		return Arrays.asList(array);
@@ -27,26 +41,28 @@ public class CollectionsChallenges {
 		return setOfStates;
 	}
 
-	static int addToList(int x, ArrayList<Integer> numList) {
+	static List<Integer> addToList(int x, ArrayList<Integer> numList) {
 		// add the x input to the array list.  return the value if succcessfully added.
 
 		if (numList.add(x)) {
-			return x;
+			return numList;
 		} else {
-			return 0;
+			return numList;
 		}
 
 	}
 
-	static ArrayList<Integer> removeFromList(int x, ArrayList<Integer> numList) {
+	static List<Integer> removeFromList(int x, ArrayList<Integer> numList) {
 		// remove the input parameter value x from the numList.
-		numList.remove(x);
+		numList.removeIf(number -> number.equals(x));
 		return numList;
 	}
 
-	static int insertIntoList(int x, int i, ArrayList<Integer> numList) {
+	static List<Integer> insertIntoList(int x, int i, ArrayList<Integer> numList) {
 		// insert the the value x at index position i for the input numList.  If the index position is outside the bounds of the current numList, add it to the end of the array list.
-		return x;
+        numList.add(i,x);
+
+        return numList;
 	}
 
 	static List<Integer> reverseNums(ArrayList<Integer> nums) {
@@ -54,7 +70,9 @@ public class CollectionsChallenges {
 		reverse the values of the nums list
 		return the reversed array list.
 		*/
-		return null;
+
+        Collections.reverse(nums);
+		return nums;
 	}
 
 	static LinkedList<String> reverseLinkedList(LinkedList<String> linkedList, boolean mutate) {
@@ -63,8 +81,7 @@ public class CollectionsChallenges {
 		example : ["red","orange","yellow","green","blue","indigo","violet"] => reverseLinkedList => [violet, indigo, blue, green, yellow, orange, red]
  */
 		if(mutate) {
-			LinkedList<String> listCopy = new LinkedList<String>();
-			listCopy = (LinkedList)linkedList.clone();
+			LinkedList<String> listCopy = new LinkedList(linkedList);
 			Collections.reverse(listCopy);
 			return listCopy;
 		}
@@ -76,7 +93,22 @@ public class CollectionsChallenges {
 
 	static ArrayList<String> removeDuplicates(ArrayList<String> names) {
 //		remove duplicates from the list of name strings.  return a new array list with the unique elements.
-		return names;
+
+        // Create a new ArrayList
+        ArrayList<String> nameList = new ArrayList<>();
+
+        // Traverse through the first list
+        for (String name : names) {
+
+            if (!nameList.contains(name)) {
+
+                nameList.add(name);
+            }
+        }
+
+        // return the new list
+
+        return nameList;
 	}
 
 	static class Person {
